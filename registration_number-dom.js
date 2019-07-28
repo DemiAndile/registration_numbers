79bb84c96cb790226daa9569500b85fb609783a1
coder <makhosandile@projectcodex.co>
2019-07-28 21:26:50 +0200

    new changes

--- registration_number-dom.js
+++ registration_number-dom.js
@@ -2,20 +2,59 @@ var enterText = document.querySelector("#enter");
 var addBtnElem = document.querySelector(".addBtn");
 var placesElem = document.querySelector(".places");
 var showBtnElem = document.querySelector(".showBtn");
-var displayRegElem = document.querySelector("#displayReg");
+// var displayRegElem = document.querySelector("#displayReg");
+var currentDiv = document.getElementById("displayReg");
+
+var registration = FactoryRegistration();
 
 
+function addItems() {
+
+    // var ul = document.getElementById("diplayReg");
+    // var  enter = document.getElementById("enter");
+    // var  li = document.createElement("li");
+    // li.setAttribute('id',enter.value);
+    // li.appendChild(document.createTextNode(enter.value))
+    // ul.appendChild(li);
+
+    if (enterText.value != "") {
+        registration.addList(enterText.value);
+        var newDiv = document.createElement("li");
+        var newContent = document.createTextNode(registration.getOnlyOne());
 
 
-function addItems(){
+        newDiv.appendChild(newContent);
+        //    console.log(newDiv)
+        currentDiv.appendChild(newDiv)
+        enterText.value = "";
+    }
 
-    var ul = document.getElementById("diplayReg");
-    var  enter = document.getElementById("enter");
-    var  li = document.createElement("li");
-    li.setAttribute('id',enter.value);
-    li.appendChild(document.createTextNode(enter.value))
-    ul.appendChild(li);
 }
 
-// addBtnElem.addEventListener('click', addItems)
+    function showTown() {
+        var liElem = document.querySelectorAll("li");
+        //var regi = currentDiv.innerHTML;
+        var radioElem = document.querySelector("input[name='myPlace']:checked")
+        if (radioElem) {
+            radioElem.value
+        }
+        for (var i = 0; i < liElem.length; i++) {
+          var regi =  liElem[i].innerHTML;
+        if (radioElem.value === "All") {
+            liElem[i].style.display = "block";
+        }
+        else if (regi.startsWith(radioElem.value)) {
+            liElem[i].style.display = "block";
+        }
+        else {
+            liElem[i].style.display = "none";
+        }
+    }
+    }
+
+
+
+
+showBtnElem.addEventListener('click', showTown)
+addBtnElem.addEventListener('click', addItems)
 
--- registration_number.js
+++ registration_number.js
@@ -1,21 +1,76 @@
 function FactoryRegistration(){
     var regNumber = [];
+     var capeTown = [];
+    // var paarl = [];
+    // var stelli = [];
+
+     let get ;
 
     function addList(reg){
-
-        if(!regNumber.includes(reg)){
-            regNumber.push(reg)
-        }
+         get = reg;
+        regNumber.push(reg)
     }
+        // function displayAll(){
+        //     return regNumber
+        // }
+
+      
+       // console.log(capeTown)
+        function cpRegNo(){
+            // return capeTown;
+        }
+
+
+        // function paarlReg(){
+        //     for (i=0; i< regNumber.length; i++) {
+        //         if (regNumber.startsWith("GP")) {
+        //             paarl.push(regNumber[i])
+        //         }
+        //     }
+        //     return Paarl;
+        // }
+
+        // function stelliReg(){
+        //     for (i=0; i < regNumber.length; i++) {
+        //         if (regNumber.startsWith("CY")) {
+        //             stelli.push(regNumber[i])
+        //         }
+        //     }
+        //     return Stellenbosch;
+        // }
+
 
     function getRegistration(){
         return regNumber;
     }
 
-    
+    function getOnlyOne(){
+        return get
+    }
+
+    function cpReg(){
+        console.log("*******")
+        for (i=0; i< regNumber.length; i++) {
+            var capeReg = regNumber[i]
+            if (capeReg.startsWith("ca")) {
+                capeTown.push(capeReg)
+            }
+        }
+        console.log(capeTown)
+         return capeTown
+    }
+
+        // regNumber.push(reg)
+        // console.log(regNumber) 
 return{
     addList,
-    getRegistration
+    getRegistration,
+    // displayAll,
+    cpReg,
+    // paarlReg,
+    // stelliReg,
+    getOnlyOne,
+    cpRegNo,
   }
 
 }
